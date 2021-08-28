ec71f72b70663d2bf3ebf18a75500ed0c26ccc9c
kamesan <47214420+kamesan1577@users.noreply.github.com>
2021-08-29 00:38:49 +0900

    complete making of game
    ゲーム部分が完成
    デザインは頼んだ

--- js/main.js
+++ js/main.js
@@ -2,24 +2,26 @@ const atomList = ["h","he","li","be","b","c","n","o","f","ne","na","mg","al","si
 atomListCapitalized = [];
 for(y of atomList){atomListCapitalized.push(capitalizeFirstLetter(y)); }
 var shownAtom = [];
+
+
 function capitalizeFirstLetter(str) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
 function Game() {
-    
     notShownAtoms = atomListCapitalized.filter(i=>shownAtom.indexOf(i)==-1)
-    console.log(atomListCapitalized);
-    console.log(shownAtom);
-    console.log(notShownAtoms);
-    for (currentAtom of atomList){
-        if(currentAtom in notShownAtoms){
-            /*ここになんかすごいコードがはいる*/
-        }
-    }
+    //console.log(atomListCapitalized);
+    //console.log(shownAtom);
+    //console.log(notShownAtoms);
+    count = 0;
+    currentAtomId = notShownAtoms[count].toLowerCase();
+    //console.log(currentAtomId);
+    document.getElementById(currentAtomId).style.backgroundColor = "red";
 }
 
 function ShowAtoms() {
+    mistake = 0;
     document.getElementById("show").disabled = true;
+    document.getElementById("show-hard").disabled = true;
     for (let i = 0; i < 10; i++){
         
         while(true){
@@ -39,17 +41,57 @@ function ShowAtoms() {
     Game();
     document.getElementById("delete").disabled = false;
 }
+function ShowAtomsHard() {
+    mistake = 0;
+    document.getElementById("show").disabled = true;
+    document.getElementById("show-hard").disabled = true;
+    Game();
+    document.getElementById("delete").disabled = false;
+}
 function DeleteAtoms() {
     for(x of atomList){
         document.getElementById(x).textContent = "";
     }
     shownAtom.length = 0;
+    for(x of notShownAtoms){
+        document.getElementById(x.toLowerCase()).style.backgroundColor = "#e0e0e0";
+    }
+    document.getElementById("congratulation").textContent = "";
+            document.getElementById("result").textContent = "";
     document.getElementById("show").disabled = false;
+    document.getElementById("show-hard").disabled = false;
     document.getElementById("delete").disabled = true;
 }
 
 function PostAnswer() {
-    console.log(String(document.getElementById("answer").value));
-    document.getElementById("current").textContent = String(document.getElementById("answer").value);
+    let answer = String(document.getElementById("answer").value);
+    
+    document.getElementById("form").reset();
+    //console.log(atomList.indexOf(currentAtomId));
+    if (atomList.indexOf(currentAtomId) === atomList.indexOf(answer.toLocaleLowerCase())){
+        
+        document.getElementById(currentAtomId).textContent = capitalizeFirstLetter(answer);
+        document.getElementById(currentAtomId).style.backgroundColor = "#e0e0e0";
+        count ++;
+
+        if (count=== notShownAtoms.length){
+            document.getElementById("congratulation").textContent = "クリアおめでとう！";
+            document.getElementById("result").textContent = String("間違えた回数:"+String(mistake)+"回");
+            return false;
+        }
+        else{
+            currentAtomId = notShownAtoms[count].toLowerCase();
+            document.getElementById(currentAtomId).style.backgroundColor = "red";
+            return false;
+        }
+        
+    }
+    else{
+        alert("違います");
+        mistake ++;
+        return false;
+    }
+    
 }
+
 
