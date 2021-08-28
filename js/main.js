11676511eec4c0b3459ecfd5498e1887bf803d52
kamesan <47214420+kamesan1577@users.noreply.github.com>
2021-08-29 01:24:12 +0900

    bug fix
    軽微なバグ修正

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-const atomList = ["h","he","li","be","b","c","n","o","f","ne","na","mg","al","si","p","s","cl","ar","k","ca"];
+const atomList = ["h", "he", "li", "be", "b", "c", "n", "o", "f", "ne", "na", "mg", "al", "si", "p", "s", "cl", "ar", "k", "ca"];
 atomListCapitalized = [];
-for(y of atomList){atomListCapitalized.push(capitalizeFirstLetter(y)); }
+for (y of atomList) { atomListCapitalized.push(capitalizeFirstLetter(y)); }
 var shownAtom = [];
 
 
 function capitalizeFirstLetter(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
+    return str.charAt(0).toUpperCase() + str.slice(1);
 }
 function Game() {
-    notShownAtoms = atomListCapitalized.filter(i=>shownAtom.indexOf(i)==-1)
+    notShownAtoms = atomListCapitalized.filter(i => shownAtom.indexOf(i) == -1)
     //console.log(atomListCapitalized);
     //console.log(shownAtom);
     //console.log(notShownAtoms);
@@ -22,22 +22,23 @@ function ShowAtoms() {
     mistake = 0;
     document.getElementById("show").disabled = true;
     document.getElementById("show-hard").disabled = true;
-    for (let i = 0; i < 10; i++){
-        
-        while(true){
-        var random = Math.floor( Math.random() * (atomList.length));
-        capitalized = capitalizeFirstLetter(atomList[random]);
-        
-        if (shownAtom.includes(capitalized)){
+    for (let i = 0; i < 10; i++) {
+
+        while (true) {
+            var random = Math.floor(Math.random() * (atomList.length));
+            capitalized = capitalizeFirstLetter(atomList[random]);
+
+            if (shownAtom.includes(capitalized)) {
+            }
+            else {
+                shownAtom.push(capitalized);
+                hoge = document.getElementById(atomList[random]);
+                hoge.textContent = shownAtom[shownAtom.length - 1]
+
+                break
+            }
         }
-        else{
-            shownAtom.push(capitalized);
-            hoge = document.getElementById(atomList[random]);
-            hoge.textContent = shownAtom[shownAtom.length - 1]
-            
-            break
-        }
-    }} 
+    }
     Game();
     document.getElementById("delete").disabled = false;
 }
@@ -49,15 +50,15 @@ function ShowAtomsHard() {
     document.getElementById("delete").disabled = false;
 }
 function DeleteAtoms() {
-    for(x of atomList){
+    for (x of atomList) {
         document.getElementById(x).textContent = "";
     }
     shownAtom.length = 0;
-    for(x of notShownAtoms){
+    for (x of notShownAtoms) {
         document.getElementById(x.toLowerCase()).style.backgroundColor = "#e0e0e0";
     }
     document.getElementById("congratulation").textContent = "";
-            document.getElementById("result").textContent = "";
+    document.getElementById("result").textContent = "";
     document.getElementById("show").disabled = false;
     document.getElementById("show-hard").disabled = false;
     document.getElementById("delete").disabled = true;
@@ -65,33 +66,38 @@ function DeleteAtoms() {
 
 function PostAnswer() {
     let answer = String(document.getElementById("answer").value);
-    
+
     document.getElementById("form").reset();
     //console.log(atomList.indexOf(currentAtomId));
-    if (atomList.indexOf(currentAtomId) === atomList.indexOf(answer.toLocaleLowerCase())){
-        
-        document.getElementById(currentAtomId).textContent = capitalizeFirstLetter(answer);
-        document.getElementById(currentAtomId).style.backgroundColor = "#e0e0e0";
-        count ++;
+    try {
+        if (atomList.indexOf(currentAtomId) === atomList.indexOf(answer.toLocaleLowerCase())) {
 
-        if (count=== notShownAtoms.length){
-            document.getElementById("congratulation").textContent = "クリアおめでとう！";
-            document.getElementById("result").textContent = String("間違えた回数:"+String(mistake)+"回");
+            document.getElementById(currentAtomId).textContent = capitalizeFirstLetter(answer);
+            document.getElementById(currentAtomId).style.backgroundColor = "#e0e0e0";
+            count++;
+
+            if (count === notShownAtoms.length) {
+                document.getElementById("congratulation").textContent = "クリアおめでとう！";
+                document.getElementById("result").textContent = String("間違えた回数:" + String(mistake) + "回");
+                return false;
+            }
+            else {
+                currentAtomId = notShownAtoms[count].toLowerCase();
+                document.getElementById(currentAtomId).style.backgroundColor = "red";
+                return false;
+            }
+
+        }
+        else {
+            alert("違います");
+            mistake++;
             return false;
         }
-        else{
-            currentAtomId = notShownAtoms[count].toLowerCase();
-            document.getElementById(currentAtomId).style.backgroundColor = "red";
-            return false;
-        }
-        
     }
-    else{
-        alert("違います");
-        mistake ++;
-        return false;
+    catch {
+        console.log("おぎゃあ");
     }
-    
 }
+
 
 
